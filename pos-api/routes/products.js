e42073e5cdecb73403b03c333747d7c59451e416
Joe Briggs <196844816+jbmgoblue@users.noreply.github.com>
2025-11-07 14:59:30 -0500

    Add products route and update server and config files

--- pos-api/routes/products.js
+++ pos-api/routes/products.js
@@ -32,4 +32,22 @@ router.post('/', async (req, res) => {
   }
 });
 
+// This will be for getting products
+router.get('/:id', async (req, res) => {
+  const {id} = req.params;
+
+  try{
+    const result = await pool.query('SELECT * from products where product_id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err){
+    console.error('Error fetching product:', err);
+    res.status(500).json({ error: 'Failed to fetch product', details: err.message});
+  }
+});
+
 module.exports = router;
--- pos-api/server.js
+++ pos-api/server.js
@@ -6,15 +6,18 @@
 const express = require('express');
 const pool = require('./db');
 const productsRouter = require('./routes/products');
-
+const getProductRouter = require('./routes/getProduct');
+const cors = require('cors');
 // Initialize the Express application, which handles HTTP requests and responses
 const app = express();	
 
 // Middleware, parse JSON bodies, necessary for POST requests
 app.use(express.json());
+app.use(cors());
 
 // Register routes for api calls
 app.use('/products', productsRouter);
+app.use('/product', getProductRouter);
 
 // Basic route to test the server, request and response
 app.get('/', (req, res) => {
@@ -22,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server, listening on 3000, use localhost:3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
