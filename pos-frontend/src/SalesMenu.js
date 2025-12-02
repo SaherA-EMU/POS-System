c69a85df2a9859ad7e9d1c3a3e15d88c28e13d38
Johney Domas <jdomas1@emich.edu>
2025-12-02 10:54:13 -0500

    Removed Reports

--- pos-frontend/src/SalesMenu.js
+++ pos-frontend/src/SalesMenu.js
@@ -74,10 +74,12 @@ export default function SalesMenu() {
                 <img src={require('./Html&Css/images/dollar-symbol.png')} alt="Dollar Sign" />
                 <span> Make a Sale</span>
             </button>
+{/*            
             <button className="Reports" onClick={ () => alert('this feature is not available yet')}>
                 <img src={require('./Html&Css/images/report.png')} alt="Reports Icon" />
                 <span> Reports </span>
             </button>
+*/}            
         </div>
         );
         }
